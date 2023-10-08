8aceb440bfd5ae6c0549e219a8ab0a41750ca37b
Owen Allison <owal7428@colorado.edu>
2023-10-08 00:33:48 -0600

    Abstracted away much of main to dedicated GameEngine class.

--- src/ooad/arcane/GameDriver.java
+++ src/ooad/arcane/GameDriver.java
@@ -1,113 +1,23 @@
 package ooad.arcane;
 
-
-import ooad.arcane.Adventurer.*;
-import ooad.arcane.Creature.*;
-import ooad.arcane.Manager.AdventurerManager;
-import ooad.arcane.Manager.CreatureManager;
-import ooad.arcane.Manager.FloorManager;
-
-import java.util.ArrayList;
-
-
 public class GameDriver {
-    static int turn = 0;
-    static int numTreasures = -1;
-    static int numCreatures = -1;
-    static int numAdventurers = -1;
-
-    static int numAdventurerWins = 0;
-    static int numCreatureWins = 0;
-
-    static boolean shouldRender;
-
     /* This whole main function is an example of abstraction because all of the complexity behind the internals
     * of the game have been abstracted away so only a few function calls are necessary to run the game. */
 
     public static void main(String[] args) {
+        int numAdventurerWins = 0;
+        int numCreatureWins = 0;
 
-        // shouldRender = true;
-        shouldRender = false;
+        boolean shouldRender = false;
 
-        // Run 30 times
-        for (int i = 0; i < 30; i++)
-        {
-            // Instantiate manager objects
-            FloorManager floorManager = new FloorManager();
+        for (int i = 0; i < 30; i++) {
+            GameEngine engine = new GameEngine(shouldRender);
+            int winner = engine.Simulate();
 
-            CreatureManager creatureManager = new CreatureManager(floorManager);
-            AdventurerManager adventurerManager = new AdventurerManager(creatureManager, floorManager);
-            creatureManager.setAdventurerManager(adventurerManager);
-
-            GameBoard renderer = new GameBoard();
-
-            /* This is an example of identity because each instance of the creature classes is
-            * distinct from one another. */
-            for (int j = 0; j < 4; j++)
-            {
-                creatureManager.addCreatures(new Aquarids(creatureManager));
-                creatureManager.addCreatures(new Fireborns(creatureManager));
-                creatureManager.addCreatures(new Terravores(creatureManager));
-                creatureManager.addCreatures(new Zephyrals(creatureManager));
-            }
-
-            for (Creature creature : creatureManager.getLivingCreatures())
-                creature.Spawn();
-
-            adventurerManager.addAdventurers(new EmberKnight(adventurerManager));
-            adventurerManager.addAdventurers(new MistWalker(adventurerManager));
-            adventurerManager.addAdventurers(new TerraVoyager(adventurerManager));
-            adventurerManager.addAdventurers(new ZephyrRogue(adventurerManager));
-
-            // Main loop
-            while(numTreasures != 50 && numAdventurers != 0 && numCreatures != 0) {
-                turn++;
-
-                // Reset values
-                numTreasures = 0;
-                numCreatures = 0;
-                numAdventurers = 0;
-
-                ArrayList<Adventurer> adventurers = adventurerManager.getAdventurers();
-
-                for (Adventurer adventurer : adventurers) {
-                    numAdventurers++;
-                    adventurer.Turn();
-                    numTreasures += adventurer.getNumTreasures();
-                }
-
-                ArrayList<Creature> creatures = creatureManager.getLivingCreatures();
-
-                for (Creature creature : creatures) {
-                    numCreatures++;
-                    creature.Turn();
-                }
-                if (shouldRender)
-                    renderer.Render(turn, floorManager);
-            }
-
-            System.out.println("...");
-
-            if (numCreatures == 0) {
-                System.out.println("Adventurers won ... All creatures killed");
+            if (winner == 1)
                 numAdventurerWins++;
-            }
-            else if (numTreasures == 50) {
-                System.out.println("Adventurers won ... All treasures found");
-                numAdventurerWins++;
-            }
-            else {
-                System.out.println("Creatures won ... All adventurers killed");
+            else
                 numCreatureWins++;
-            }
-
-            // Reset values
-            numTreasures = -1;
-            numAdventurers = -1;
-            numCreatures = -1;
-
-            if (shouldRender)
-                break;
         }
 
         if (!shouldRender) {
@@ -115,6 +25,8 @@ public class GameDriver {
             System.out.println("The creatures won " + numCreatureWins + " times");
         }
 
+        System.out.println();
+        System.out.println("...");
         System.out.println("Game Finished.");
     }
 
---
+++ src/ooad/arcane/GameEngine.java
@@ -0,0 +1,93 @@
+package ooad.arcane;
+
+
+import ooad.arcane.Adventurer.*;
+import ooad.arcane.Creature.*;
+import ooad.arcane.Manager.AdventurerManager;
+import ooad.arcane.Manager.CreatureManager;
+import ooad.arcane.Manager.FloorManager;
+
+import java.util.ArrayList;
+
+public class GameEngine {
+    private int turn = 0;
+    private int numTreasures = -1;
+    private int numCreatures = -1;
+    private int numAdventurers = -1;
+    private final boolean shouldRender;
+
+    FloorManager floorManager = new FloorManager();
+    CreatureManager creatureManager = new CreatureManager(floorManager);
+    AdventurerManager adventurerManager = new AdventurerManager(creatureManager, floorManager);
+    GameBoard renderer = new GameBoard();
+
+    public GameEngine(boolean shouldRender) {
+        this.shouldRender = shouldRender;
+
+        creatureManager.setAdventurerManager(adventurerManager);
+
+        /* This is an example of identity because each instance of the creature classes is
+         * distinct from one another. */
+        for (int j = 0; j < 4; j++)
+        {
+            creatureManager.addCreatures(new Aquarids(creatureManager));
+            creatureManager.addCreatures(new Fireborns(creatureManager));
+            creatureManager.addCreatures(new Terravores(creatureManager));
+            creatureManager.addCreatures(new Zephyrals(creatureManager));
+        }
+
+        for (Creature creature : creatureManager.getLivingCreatures())
+            creature.Spawn();
+
+        adventurerManager.addAdventurers(new EmberKnight(adventurerManager));
+        adventurerManager.addAdventurers(new MistWalker(adventurerManager));
+        adventurerManager.addAdventurers(new TerraVoyager(adventurerManager));
+        adventurerManager.addAdventurers(new ZephyrRogue(adventurerManager));
+    }
+
+    // This method returns 1 if adventurers win, 0 if creatures win
+    public int Simulate() {
+        // Main loop
+        while(numTreasures != 50 && numAdventurers != 0 && numCreatures != 0) {
+            turn++;
+
+            // Reset values
+            numTreasures = 0;
+            numCreatures = 0;
+            numAdventurers = 0;
+
+            ArrayList<Adventurer> adventurers = adventurerManager.getAdventurers();
+
+            for (Adventurer adventurer : adventurers) {
+                numAdventurers++;
+                adventurer.Turn();
+                numTreasures += adventurer.getNumTreasures();
+            }
+
+            ArrayList<Creature> creatures = creatureManager.getLivingCreatures();
+
+            for (Creature creature : creatures) {
+                numCreatures++;
+                creature.Turn();
+            }
+            if (shouldRender)
+                renderer.Render(turn, floorManager);
+        }
+
+        System.out.println("...");
+
+        if (numCreatures == 0) {
+            System.out.println("Adventurers won ... All creatures killed");
+            return 1;
+        }
+        else if (numTreasures == 50) {
+            System.out.println("Adventurers won ... All treasures found");
+            return 1;
+        }
+        else {
+            System.out.println("Creatures won ... All adventurers killed");
+            return 0;
+        }
+    }
+
+}
